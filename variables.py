8d08495c2bba50b60b8d1bcbff489faf41bc5aeb
Janio Isacura <janioisacura@gmail.com>
2026-06-22 18:52:01 -0300

    Add variables.py to handle user input for name and age

---
+++ variables.py
@@ -0,0 +1,16 @@
+"""
+persona = "Juan"
+print(persona)
+
+persona = "Mariana"
+print(persona)
+
+print(edad)
+
+edad2 = 35
+print(edad + edad2)
+"""
+nombre = (input("tu nombre: "))
+edad = (input("tu edad: "))
+#print("Tu nombre es: " + nombre + ", y tienes " + str(edad) + " años")
+print(f"Tu nombre es: {nombre}, y tienes {edad} años")
