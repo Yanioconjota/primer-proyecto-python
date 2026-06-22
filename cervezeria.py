bf93a19208ddaaee4bf25452d8bfda0b5fea236e
Janio Isacura <janioisacura@gmail.com>
2026-06-22 17:23:14 -0300

    Initial commit

---
+++ cervezeria.py
@@ -0,0 +1,9 @@
+#Ejercicio
+"""
+Vas a crear un código en Python que le pida a tu amigo que responda dos preguntas que
+requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras
+combinadas, para formar una marca creativa.
+"""
+print("El nombre de tu cerveza es: " + '\n' + '"'
+      + (input('Escribe el apellido del jugador más grande del mundo: ')) + ' '
+      + (input("Ahora ingresa el nombre de tu mascota: ")) + '"')
---
+++ primerScript.py
@@ -0,0 +1,12 @@
+# Print
+"""
+print("Esta es la primera línea \nEsta es la Segunda línea")
+print("\tEsta es la tercera línea")
+print("\t\tEsta es la cuarta línea")
+print("Este signo: \\ es una barra invertida")
+"""
+#Input
+"""
+print('Hola ' + (input("tu nombre: ")) + ' ' + (input("tu apellido: ")) + '!')
+"""
+print((input('Escribe tu nombre:')) + ' ' + (input('Escribe tu apellido:')))
